836d9d5c704056e7645eb8d17632b8d2aee8d258
조민규(Mingyu Cho  웹플랫폼개발팀)(alucard) <alucard@ncsoft.com>
2015-08-05 16:39:14 +0900

    add library swipemenu

--- app/src/main/java/kr/mamo/travelpoint/fragment/FragmentTravelHistory.java
+++ app/src/main/java/kr/mamo/travelpoint/fragment/FragmentTravelHistory.java
@@ -5,6 +5,8 @@ import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.net.Uri;
@@ -18,7 +20,12 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -40,7 +47,7 @@ import kr.mamo.travelpoint.db.domain.TravelPoint;
 import kr.mamo.travelpoint.util.GPSTracker;
 
 public class FragmentTravelHistory extends Fragment implements FragmentTravelPoint.OnClickTravelPointListener {
-    private ListView travelHistoryList;
+    private SwipeMenuListView travelHistoryList;
     private TravelHistoryAdapter travelHistoryAdapter;
 
     OnCaptureImageListener captureImageListener;
@@ -57,10 +64,12 @@ public class FragmentTravelHistory extends Fragment implements FragmentTravelPoi
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_travel_history, container, false);
         googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.travel_point_map)).getMap();
-        travelHistoryList = (ListView) view.findViewById(R.id.travel_history_list);
+        travelHistoryList = (SwipeMenuListView) view.findViewById(R.id.travel_history_list);
         travelHistoryAdapter = new TravelHistoryAdapter();
         travelHistoryList.setAdapter(travelHistoryAdapter);
         travelHistoryList.setOnItemClickListener(itemClickListener);
+        travelHistoryList.setMenuCreator(creator);
+        travelHistoryList.setOnMenuItemClickListener(swipeMenuListener);
         travelPointDescription = (TextView) view.findViewById(R.id.travel_point_description);
         camera = (Button) view.findViewById(R.id.travel_history_camera);
         camera.setOnClickListener(cameraClickListener);
@@ -133,6 +142,60 @@ public class FragmentTravelHistory extends Fragment implements FragmentTravelPoi
 
         }
     }
+
+    SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+        @Override
+        public void create(SwipeMenu menu) {
+            // create "open" item
+            SwipeMenuItem openItem = new SwipeMenuItem(
+                    getActivity());
+            // set item background
+            openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                    0xCE)));
+            // set item width
+            openItem.setWidth(90);
+            // set item title
+            openItem.setTitle("Open");
+            // set item title fontsize
+            openItem.setTitleSize(18);
+            // set item title font color
+            openItem.setTitleColor(Color.WHITE);
+            // add to menu
+            menu.addMenuItem(openItem);
+
+            // create "delete" item
+            SwipeMenuItem deleteItem = new SwipeMenuItem(
+                    getActivity());
+            // set item background
+            deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                    0x3F, 0x25)));
+            // set item width
+            deleteItem.setWidth(90);
+            // set a icon
+            deleteItem.setIcon(R.drawable.penguin);
+            // add to menu
+            menu.addMenuItem(deleteItem);
+        }
+    };
+
+    private SwipeMenuListView.OnMenuItemClickListener swipeMenuListener = new SwipeMenuListView.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+            switch (index) {
+                case 0:
+                    // open
+                    Toast.makeText(getActivity(), "open", Toast.LENGTH_LONG).show();
+                    break;
+                case 1:
+                    // delete
+                    Toast.makeText(getActivity(), "delete", Toast.LENGTH_LONG).show();
+                    break;
+            }
+            // false : close the menu; true : not close the menu
+            return false;
+        }
+    };
 
     private View.OnClickListener cameraClickListener = new View.OnClickListener() {
 
