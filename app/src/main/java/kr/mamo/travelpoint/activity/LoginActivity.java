51f387582d81a635b1946ae4bd9e0db7d7514b59
조민규(Mingyu Cho  웹플랫폼개발팀)(alucard) <alucard@ncsoft.com>
2015-08-13 14:41:46 +0900

    facebook login

--- app/src/main/java/kr/mamo/travelpoint/activity/LoginActivity.java
+++ app/src/main/java/kr/mamo/travelpoint/activity/LoginActivity.java
@@ -8,6 +8,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,6 +18,20 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookActivity;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -33,10 +48,15 @@ public class LoginActivity extends Activity {
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
+    private LoginButton facebook;
+    CallbackManager callbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         boolean auto = prefs.getBoolean(Constants.Preference.Account.AUTO_LOGIN, true);
@@ -57,7 +77,7 @@ public class LoginActivity extends Activity {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
+                    attemptLocalLogin();
                     return true;
                 }
                 return false;
@@ -68,13 +88,61 @@ public class LoginActivity extends Activity {
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                attemptLocalLogin();
             }
         });
 
+        facebook = (LoginButton) findViewById(R.id.facebook_sign_in_button);
+        facebook.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startFacebookActivity();
+            }
+        });
+        facebook.setReadPermissions("email", "public_profile");
+        // If using in a fragment
+//        facebook.setFragment(this);
+        // Other app specific specialization
+
+        callbackManager = CallbackManager.Factory.create();
+        // Callback registration
+        facebook.registerCallback(callbackManager, facebookCallback);
+
+        AccessToken at = AccessToken.getCurrentAccessToken();
+        if (null != at) {
+            Log.i(Constants.LOGCAT_TAGNAME, "at : " + at.getToken());
+
+            GraphRequest request = GraphRequest.newMeRequest(at, new GraphRequest.GraphJSONObjectCallback() {
+                @Override
+                public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse) {
+                    Log.i(Constants.LOGCAT_TAGNAME, "response : " + graphResponse.toString());
+
+                    try {
+                        int responseCode = graphResponse.getJSONObject().getInt("responseCode");
+                    } catch (JSONException e) {
+
+                    }
+
+                }
+            });
+
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "email");
+            request.setParameters(parameters);
+            request.executeAsync();
+        }
     }
 
-    public void attemptLogin() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    public void attemptLocalLogin() {
         if (mAuthTask != null) {
             return;
         }
@@ -102,6 +170,12 @@ public class LoginActivity extends Activity {
         }
     }
 
+    private void startFacebookActivity() {
+        Intent intent = new Intent(this, FacebookActivity.class);
+        startActivity(intent);
+//        finish();
+    }
+
     private void startMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
@@ -127,6 +201,26 @@ public class LoginActivity extends Activity {
 
         mEmailView.setAdapter(adapter);
     }
+
+    FacebookCallback<LoginResult> facebookCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            Log.i(Constants.LOGCAT_TAGNAME, "facebook success :");
+            AccessToken token = loginResult.getAccessToken();
+        }
+
+        @Override
+        public void onCancel() {
+            Log.i(Constants.LOGCAT_TAGNAME, "facebook cancel :");
+
+        }
+
+        @Override
+        public void onError(FacebookException exception) {
+            Log.i(Constants.LOGCAT_TAGNAME, "facebook error :");
+
+        }
+    };
 
     /**
      * Represents an asynchronous login/registration task used to authenticate
---
+++ app/src/main/java/kr/mamo/travelpoint/auth/LoginInterface.java
@@ -0,0 +1,7 @@
+package kr.mamo.travelpoint.auth;
+
+/**
+ * Created by mentalmamo on 15. 8. 13..
+ */
+public interface LoginInterface {
+}
